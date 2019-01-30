ad0f546017c7b1cf41d311944d95d55a1aa55f5a
Ronald Cargua <ronald_96c@hotmail.com>
2019-01-30 11:22:18 -0500

    hh

---
+++ Proyecto/baseCoop/api/controllers/BancoController.js
@@ -0,0 +1,12 @@
+/**
+ * BancoController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
