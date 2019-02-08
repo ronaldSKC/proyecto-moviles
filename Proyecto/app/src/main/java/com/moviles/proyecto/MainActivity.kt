ad800c21d0d9d005a103165dec7d815bba1332f7
Ronald <ronald_96c@hotmil.com>
2019-02-08 16:39:22 -0500

    ultimo

--- Proyecto/app/src/main/java/com/moviles/proyecto/MainActivity.kt
+++ Proyecto/app/src/main/java/com/moviles/proyecto/MainActivity.kt
@@ -1,22 +1,22 @@
 package com.moviles.proyecto
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_http.setOnClickListener {
-            this.irActivityHttp()
-        }
+
     }
-    fun irActivityHttp (){
-        val intentHttp = Intent(this, HttpActivity::class.java)
-        startActivity(intentHttp)
-    }
+
+     fun irARegistrarUsuario(){
+      val intent = Intent(this, RegistrarUsuarioActivity::class.java)
+         startActivity(intent)
+     }
 }
---
+++ Proyecto/app/src/main/java/com/moviles/proyecto/RegistrarUsuarioActivity.kt
@@ -0,0 +1,12 @@
+package com.moviles.proyecto
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrarUsuarioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar_usuario)
+    }
+}
